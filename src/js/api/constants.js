3111ec905228086e46acd8a18492c50df5d22364
RobertDacian <tennerdigitalart@gmail.com>
2022-11-29 11:59:43 -0300

    Added auth

---
+++ src/js/api/constants.js
@@ -0,0 +1,13 @@
+export const API_BASE = "https://api.noroff.dev/api/v1/auction";
+export const API_AUTH_REGISTER = `${API_BASE}/auth/register`;
+export const API_AUTH_LOGIN = `${API_BASE}/auth/login`;
+export const API_LISTINGS = `${API_BASE}/listings`;
+export const API_PROFILES = `${API_BASE}/profiles`;
+
+// console.log([
+// 	API_BASE,
+// 	API_AUTH_REGISTER,
+// 	API_AUTH_LOGIN,
+// 	API_LISTINGS,
+// 	API_PROFILES,
+// ]);
